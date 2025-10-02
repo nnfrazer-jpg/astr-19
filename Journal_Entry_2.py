94bab8bf274c17c827514d01be8271260a32a475
nnfrazer-jpg <nnfrazer@ucsc.edu>
2025-10-02 11:57:55 -0700

    Journal Entry #2 for Coding Journal #1

---
+++ Journal_Entry_2.py
@@ -0,0 +1,16 @@
+
+ #  Write a Python that prints the sum of two floating point numbers, the difference between two integers, and the product of a floating point number and an integer. In each case, have the program print out the data type of the resulting answer.
+
+x1 = 3.3
+x2 = 9.9
+s = x1 + x2
+
+x3 = 8
+x4 = 4
+d = x3 / x4
+
+x5 = 1.2
+x6 = 4
+p = x5 * x6
+
+print(type(s), type(d), type(p))
